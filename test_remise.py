ccb8a35db80b14eb527e62efa00f8381d48a250c
Amine <mohamedamine.ma919@gmail.com>
2024-02-21 00:11:27 +0100

    test add coupon

--- test_remise.py
+++ test_remise.py
@@ -13,3 +13,20 @@ def stock():
     panier.ajouterArticle("viande", 1.3, 10, date1)
     panier.ajouterArticle("tomate", 2, 4, date2)
     return panier
+
+
+
+def test_ajouter_coupon(stock, capsys):
+    with pytest.raises(ValueError) as e:
+        stock.addCoupon("NADA", 0, "viande")
+    assert str(e.value) == "La réduction doit être supérieure à 0."
+
+    stock.addCoupon("CODE20", 20, "viande")
+
+    with pytest.raises(ValueError) as e:
+        stock.addCoupon("CODE20", 20, "tomate")
+    assert str(e.value) == "Ce coupon a déjà été appliqué a un article."
+
+    with pytest.raises(ValueError) as e:
+        stock.addCoupon("NEWCODE", 20, "viande")
+    assert str(e.value) == "L'article possède déjà un coupon."
