9119de6d3bc38057a51465dcb48ff67f5fed69e4
Amine <mohamedamine.ma919@gmail.com>
2024-02-20 23:39:54 +0100

    create classs coupon

--- panier.py
+++ panier.py
@@ -2,6 +2,12 @@ from datetime import datetime
 from article import Article
 
 
+class Coupon:
+    def __init__(self, name, reduction, article=None):
+        self.name = name
+        self.reduction = reduction
+        self.article = article
+
 class Panier:
     def __init__(self):
         self.articles = []
@@ -36,6 +42,18 @@ class Panier:
                     self.historique_stock.update(nom_article, quantite, datetime.now().strftime("%Y-%m-%d"), "remove")
                 return
         raise ValueError("L'article n'est pas actuellement en stock.")
-        
+    
 
+    def addCoupon(self, name, reduction, article=None):
+        if article is None:
+            if self.reduction is not None:
+                raise ValueError("Une réduction a déjà été appliquée.")
+            self.reduction = Coupon(name, reduction)
+        if reduction <= 0:
+            raise ValueError("La réduction doit être supérieure à 0.")
+        if any(coupon for coupon in self.coupons if coupon.article == article):
+            raise ValueError("Le coupon a etait deja appliqué sur cet article.")
+        if any(coupon for coupon in self.coupons if coupon.name == name):
+            raise ValueError("Ce coupon a déjà été appliqué a un article.")
+        self.coupons.append(Coupon(name, reduction, article))
 
