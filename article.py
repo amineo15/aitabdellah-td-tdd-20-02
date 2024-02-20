ec07f1883e86ff9a09757d57eeb2a89f772d950c
Amine <127109108+amineo15@users.noreply.github.com>
2024-02-20 22:46:58 +0100

    Update article.py

---
+++ article.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+
+class Article:
+    def __init__(self, nom, prix, quantite, date_expiration):
+        self.nom = nom
+        self.prix = prix
+        self.quantite = quantite
+        self.date_expiration = datetime.strptime(date_expiration, "%Y-%m-%d")
+
+    def getNom(self):
+        return self.nom
+    
+    def getPrix(self):
+        return self.prix
+    
+    def getQuantite(self):
+        return self.quantite
+
+    def getDateExpiration(self):
+        return self.date_expiration
